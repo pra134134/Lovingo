dabfa7d4442f93a4c12ceba09cb52ad8020645c0
pra134134 <pra134134@gmail.com>
2025-09-30 08:54:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// JavaScript for Medium landing page
+
+// Currently no interactive functionality needed.
+// This file can be extended for future enhancements.
+
+console.log('Medium landing page script loaded.');
